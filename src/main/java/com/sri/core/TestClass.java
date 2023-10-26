efcde16ce740e87ad9f99a3df8b42e25f53ceb16
amma <usrinat908@gmail.com>
2023-10-26 06:57:19 -0400

    Test

--- src/main/java/com/sri/core/TestClass.java
+++ src/main/java/com/sri/core/TestClass.java
@@ -2,5 +2,9 @@ package com.sri.core;
 
 public class TestClass {
 
-  //  System.out.println("hiiiiii");
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        System.out.println("Welcome");
+    }
+
 }
